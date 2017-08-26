ab11141e20688d6f49c49262b5b6c1f070b16cb0
Ryan Osten <ostenrj@gmail.com>
2017-08-26 11:02:53 -0400

    parsing API response

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,16 +3,28 @@
 const express = require('express');
 const router = express.Router();
 //const request = require('request');
-const rp = require('request-promise');
+const request = require('request-promise');
 
 //handle get request to '/ route
 router.get('/', (req, res) => {
-  rp('https://lcboapi.com/products?access_key=MDowZTFmMjc3OC04ODY3LTExZTctYjc2Ni01M2UxOGZkOWJiYWY6SHR5NGFDU0FldmFPVnJCdDU1SWJWTjZXVmh3WTRHY1lFWkZy&q=Beau%27s+All+Natural+Brewing&where=is_seasonal')
+  let beers = [];
+
+  //make api call
+  request('https://lcboapi.com/products?access_key=MDowZTFmMjc3OC04ODY3LTExZTctYjc2Ni01M2UxOGZkOWJiYWY6SHR5NGFDU0FldmFPVnJCdDU1SWJWTjZXVmh3WTRHY1lFWkZy&q=Beau%27s+All+Natural+Brewing&where=is_seasonal')
     .then(function(response){
-      console.log(response);
+      let beer_json = JSON.parse(response).result;
+
+      for(let item of beer_json){
+        if(item.tertiary_category != "Taster Pack"){
+          let beer_obj = {}
+          beer_obj.name = item.name;
+          beer_obj.thumbnail = item.image_thumb_url;
+          beers.push(beer_obj);
+        }
+      }
+
     });
   res.render('index');
-  //make api call
   //package object
   //pass object to view
   //render view
