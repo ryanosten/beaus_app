4cbcf867b2d9998ff56260f29db7d25d536ada8b
Ryan Osten <ostenrj@gmail.com>
2017-08-27 18:00:59 -0400

    successfully rendering template and added bootstrap classes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,27 +7,22 @@ const request = require('request-promise');
 
 //handle get request to '/ route
 router.get('/', (req, res) => {
-  let beers = [];
-
   //make api call
   request('https://lcboapi.com/products?access_key=MDowZTFmMjc3OC04ODY3LTExZTctYjc2Ni01M2UxOGZkOWJiYWY6SHR5NGFDU0FldmFPVnJCdDU1SWJWTjZXVmh3WTRHY1lFWkZy&q=Beau%27s+All+Natural+Brewing&where=is_seasonal')
     .then(function(response){
+      let beers = [];
       let beer_json = JSON.parse(response).result;
 
       for(let item of beer_json){
-        if(item.tertiary_category != "Taster Pack"){
+        if(item.tertiary_category !== "Taster Pack" && item.image_thumb_url !== null){
           let beer_obj = {}
           beer_obj.name = item.name;
           beer_obj.thumbnail = item.image_thumb_url;
           beers.push(beer_obj);
         }
       }
-
+    res.render('index', {beers: beers});
     });
-  res.render('index');
-  //package object
-  //pass object to view
-  //render view
 });
 
 module.exports = router;
