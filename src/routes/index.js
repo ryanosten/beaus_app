8049b1e25244e436a93ad1521736c7b1ff0ba604
Ryan Osten <ostenrj@gmail.com>
2017-08-28 23:04:56 -0400

    added description to beer object

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,6 +21,7 @@ router.get('/', (req, res) => {
           beers.push(beer_obj);
         }
       }
+      console.log(beer_json);
     res.render('index', {beers: beers});
     });
 });
@@ -31,10 +32,14 @@ router.get('/products/:id', (req, res) => {
     .then(function(response){
       let beer_json = JSON.parse(response).product;
       let stores_json = JSON.parse(response).result;
+      if(beer_json.tasting_note === null) {
+        beer_json.tasting_note = 'Sorry, we were too lazy to create a description for this beer. But trust us, it\'s fucking delicious!'
+      }
       let beer = {
         name: beer_json.name,
         image: beer_json.image_url,
-        description: beer_json.description
+        //used tasting note bc description was null for all objects returned by API
+        description: beer_json.tasting_note
       }
 
       let stores = [];
